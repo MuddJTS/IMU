eb87af3326845c9697ea181abff310ab76cf08a7
Nico Espinosa Dice <nespinosadice@hmc.edu>
2020-04-13 23:34:20 -0400

    Organized data files

--- MPU9250 Code/basic_sending_12.14.19.c/basic_sending_12.14.19.c.ino
+++ MPU9250 Code/basic_sending_12.14.19.c/basic_sending_12.14.19.c.ino
@@ -30,6 +30,14 @@
  Data out (DOUT pin11) - - - - - - - serial data in (SDA/SDI)
  Clock (SCK - pin13) - - - - - - - - Clock (SCLK/SCL)
  Chip Select (CS) - - - - - Chip select (CS)
+
+ The circuit for SD:
+ * analog sensors on analog ins 0, 1, and 2
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11, pin 7 on Teensy with audio board
+ ** MISO - pin 12
+ ** CLK - pin 13, pin 14 on Teensy with audio board
+ ** CS - pin 4,  pin 10 on Teensy with audio board
  
  */
 
@@ -40,6 +48,24 @@
 
 #include "quaternionFilters.h"
 #include "MPU9250.h"
+#include <SD.h>
+#include <SPI.h>
+
+// On the Ethernet Shield, CS is pin 4. Note that even if it's not
+// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
+// 53 on the Mega) must be left as an output or the SD library
+// functions will not work.
+
+// change this to match your SD shield or module;
+// Arduino Ethernet shield: pin 4
+// Adafruit SD shields and modules: pin 10
+// Sparkfun SD shield: pin 8
+// Teensy audio board: pin 10
+// Teensy 3.5 & 3.6 on-board: BUILTIN_SDCARD
+// Wiz820+SD board: pin 4
+// Teensy 2.0: pin 0
+// Teensy++ 2.0: pin 20
+const int chipSelect = 10;
 
 #define AHRS false         // Set to false for basic data read
 #define SerialDebug true  // Set to true to get Serial output for debugging
@@ -60,9 +86,25 @@ MPU9250 myIMU(CSpin, SPIport, SPIspeed);
 // ______________________________________ SETUP _________________________________________________________________________
 void setup()
 {
+  //SPI.setMOSI(7);  // Audio shield has MOSI on pin 7
+  //SPI.setSCK(14);  // Audio shield has SCK on pin 14
   
-  Serial.begin(38400);
-  while(!Serial){};
+   // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  Serial.print("Initializing SD card...");
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
 
   // Set up the interrupt pin, its set as active high, push-pull
   pinMode(intPin, INPUT);
@@ -314,4 +356,20 @@ void loop()
       myIMU.sum = 0;
     } // if (myIMU.delt_t > 500)
   } // if (AHRS)
+  
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalogtest1.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println("testing");
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println("testing");
+  }  
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  } 
 }
